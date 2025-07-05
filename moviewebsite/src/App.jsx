5ce1b70d73a15f113d5713cc1ae9a1749fef78b5
ifeanyi chukwu <emmanueludebu@gmail.com>
2025-07-05 23:25:28 +0100

    creating search component

--- moviewebsite/src/App.jsx
+++ moviewebsite/src/App.jsx
@@ -1,16 +1,22 @@
 
 import './App.css'
+import Search from "./component/Search.jsx";
+import {useState} from "react";
 
 function App() {
+
+    const [searchText, setSearchText] = useState("");
+
 
   return (
       <main>
         <div className="pattern">
           <div className="wrapper">
             <header>
+                <img src="./hero.png" alt=""/>
               <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
             </header>
-            <p>Search</p>
+            <Search />
           </div>
         </div>
       </main>
