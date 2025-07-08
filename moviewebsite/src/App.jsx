eb09eeed0c87bf4737b80c21d3b5a57939478063
ifeanyi chukwu <emmanueludebu@gmail.com>
2025-07-08 16:43:51 +0100

    initializing Api call

--- moviewebsite/src/App.jsx
+++ moviewebsite/src/App.jsx
@@ -1,13 +1,43 @@
 
 import './App.css'
 import Search from "./component/Search.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+
+const API_BASE_URL ='https://api.themoviedb.org/3'
+const API_KEY = import.meta.env.VITE_TMDB_KEY;
+
+const API_OPTIONS = {
+    method: 'GET',
+    header:{
+        'Accept': 'application/json',
+        Authorization: `Bearer ${API_KEY}`,
+    }
+}
+
 
 function App() {
 
-    const [searchText, setSearchText] = useState("");
+    const [searchTerm, setSearchTerm] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    
+    const fetchMovies = async () => {
+        try{
+            
+        } catch (error) {
+            console.error(`Error fetching movies: ${error}`);
+            setErrorMessage('Error fetching movies. Please try again later.');
+            
+        }
+        
+    }
+
+    useEffect(() => {
 
 
+
+    }, [
+        setSearchTerm
+])
   return (
       <main>
         <div className="pattern">
@@ -15,12 +45,17 @@ function App() {
             <header>
                 <img src="./hero.png" alt=""/>
               <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+            <Search  searchTerm={searchTerm} setSearchText={setSearchTerm} />
             </header>
-            <Search />
+              <section className="all-movies">
+                  <h2>All Movies</h2>
+                  {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+              </section>
           </div>
         </div>
       </main>
   )
 }
+
 
 export default App
--- moviewebsite/src/component/Search.jsx
+++ moviewebsite/src/component/Search.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const Search = () => {
+const Search = ({searchTerm, setSearchTerm} ) => {
     return (
-        <div className=' text-white text-3xl'>Search</div>
+        <div className='search'>
+            <div>
+                <img src="search.svg" alt="search-icon"/>
+                <input type="text"
+                       value={setSearchTerm}
+                       onChange={(e) => setSearchTerm(e.target.value)}
+                       placeholder="You can Search through thousands of movies..."/>
+
+            </div>
+
+        </div>
     )
 }
 export default Search
